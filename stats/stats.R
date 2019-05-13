734579feef542acfd368348f8fee0750303722a5
Georgios <gdkoutsovoulos@gmail.com>
2019-05-13 11:18:09 +0200

    update stats.R
    Added N50 calculation

--- stats/stats.R
+++ stats/stats.R
@@ -1,3 +1,7 @@
 #!/usr/bin/env Rscript
 a <- scan(file("stdin"), c(0), quiet=TRUE);
-cat("Min:", min(a),"Max:", max(a), "Mean:", mean(a), "Median:",  median(a), "SD:", sd(a), "Sum:", sum(a), "\n");
+tmp = a;
+tmp2 <- rev( sort(tmp) );
+tmp3 <- cumsum(tmp2) <= sum(tmp)/2;
+n50 = tmp2[ sum(tmp3) ];
+cat("Min:", min(a),"Max:", max(a), "Mean:", mean(a), "Median:",  median(a), "SD:", sd(a), "Sum:", sum(a), "N50:", n50, "\n");
